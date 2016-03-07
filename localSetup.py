134e8de00443635eb34438e93a0c74cbb1ef0027
Mayank Home Workstation <mayank@ws.home>
2016-03-07 10:27:18 +0530

    moving from nfs to localhome at janelia

--- localSetup.py
+++ localSetup.py
@@ -6,10 +6,10 @@
 import socket
 import sys,re
 
-if re.search('janelia.priv',socket.gethostname()):
+if re.search('verman-ws1.janelia.priv',socket.gethostname()):
     sys.path.append('/groups/branson/home/kabram/bransonlab/pose_estimation/caffe/python')
     sys.path.append('/groups/branson/home/kabram/bransonlab/pyutils')
-    bdir = '/groups/branson/home/kabram/bransonlab/pose_estimation/poseTF/'
+    bdir = '/localhome/kabram/poseTF/'
 else:
     sys.path.append('/home/mayank/work/caffe/python')
     sys.path.append('/home/mayank/work/pyutils')
